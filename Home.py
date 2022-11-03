61e2ae5a61ea7f8b25244d5fdb6c2fc6c5362ae4
William Mattingly <62964060+wjbmattingly@users.noreply.github.com>
2022-11-03 22:45:49 +0100

    improved speed with to_feather

--- Home.py
+++ Home.py
@@ -17,77 +17,39 @@ st.set_page_config(layout="wide")
 # st.title("BAP SAHA-Infocom Map")
 def grab_uniques(df, column):
     items = []
+    df = df[df[column].notna()]
     for idx, row in df.iterrows():
-        if pd.isnull(row[column]):
-            pass
-        else:
-            for item in row[column].split("|"):
-                if item not in items:
-                    items.append(item)
+        for item in row[column]:
+            if item not in items and item != None:
+                items.append(item)
     items.sort()
     return items
 @st.cache(allow_output_mutation=True)
 def load_data():
-    df = pd.read_csv("data/data_cleaned.csv")
-    # df = df[:1000]
-    df = df[df['place'].notna()]
-    df = df[df['long'].notna()]
-    df = df[df['lat'].notna()]
-    df.index = df.object_id
-    orgs = grab_uniques(df, "org")
-    places = grab_uniques(df, "place")
-    homelands = grab_uniques(df, "homeland")
-    provinces = grab_uniques(df, "province")
-    hrvs = grab_uniques(df, "hrv")
+    data = pd.read_feather("data/full_data")
+    dates_only = pd.read_feather("data/dates_only")
+    orgs = grab_uniques(data, "org")
+    places = grab_uniques(data, "place")
+    homelands = grab_uniques(data, "homeland")
+    provinces = grab_uniques(data, "province")
+    hrvs = grab_uniques(data, "hrv")
 
-    data = []
-    dates_only = []
-    for idx, row in df.iterrows():
-        if "-" not in str(row.long):
-            # try:
-            # st.write(row.dates)
-            # if dates_checkbox:
-            if pd.isnull(row.date):
-                # if dates_checkbox == False:
-                name = f"{row.full_name}"
-                coords = [float(row.long), float(row.lat)]
-                place = row.place
-                description = row.description
-                data.append({"name": str(name),
-                "place": str(place),
-                "description": str(description),
-                "coordinates": coords,
-                "date": "Date: " + "Unknown",
-                "id": row.object_id})
-                # pass
-            else:
+    return orgs, places, homelands, provinces, hrvs, data, dates_only
 
-                dates = row.date.split("|")
-                # st.write(dates)
-                for date in dates:
-                    date = dateparser.parse(date)
-                    if date != None:
-                        date = date.date()
-                        # if (dates_checkbox==False) or (date > start_date and date < end_date):
-                        if pd.isnull(row.long) or pd.isnull(row.lat):
-                            pass
-                        else:
-                            name = f"{row.full_name}"
-                            coords = [float(row.long), float(row.lat)]
-                            place = row.place
-                            description = row.description
-                            temp = {"name": str(name),
-                            "place": str(place),
-                            "description": str(description),
-                            "coordinates": coords,
-                            "date": "Date: " + str(date),
-                            "date_time": date,
-                            "id": row.object_id}
-                            data.append(temp)
-                            dates_only.append(temp)
-    return df, orgs, places, homelands, provinces, hrvs, data, dates_only
-
-df, orgs, places, homelands, provinces, hrvs, full_data, dates_only = load_data()
+def get_new_lists(df, ignore_column):
+    columns = [["org", "orgs"], ["place", "places"], ["homeland", "homelands"], ["province", "provinces"], ["hrv", "hrvs"]]
+    for column, ss_key in columns:
+        if column != ignore_column:
+            temp_df = df[df[column].notna()]
+            hits = temp_df[column].tolist()
+            uniques = []
+            for hit in hits:
+                for item in hit.split("|"):
+                    if item not in uniques and item != None:
+                        uniques.append(item)
+            uniques.sort()
+            st.session_state[ss_key] = uniques
+orgs, places, homelands, provinces, hrvs, full_data, dates_only = load_data()
 
 # param_columns = st.columns(2)
 # layers_option = param_columns[0].checkbox("Multiple Layers?", )
@@ -95,19 +57,30 @@ df, orgs, places, homelands, provinces, hrvs, full_data, dates_only = load_data(
 # if layers_option:
 #     num_layers = st.number_input("How many Layers?", 1, 3)
 #     layers_select = st.selectbox("Which Layers", [f"Layer {i+1}" for i in range(num_layers)])
+def change_sessions(name, data_list, refresh=False):
+    if refresh==True:
+        st.session_state[name] = data_list
+    if name not in st.session_state:
+        st.session_state[name] = data_list
 
 
-selected_orgs = st.sidebar.multiselect("Select Organization(s)", orgs)
-selected_places = st.sidebar.multiselect("Select Place(s)", places)
-selected_homelands = st.sidebar.multiselect("Select Homeland(s)", homelands)
-selected_provinces = st.sidebar.multiselect("Select Province(s)", provinces)
-selected_hrvs = st.sidebar.multiselect("Select HRV(s)", hrvs)
+
+data_lists = [["orgs", orgs], ["places", places], ["homelands", homelands], ["provinces", provinces], ["hrvs", hrvs]]
+for name, data_list in data_lists:
+    change_sessions(name, data_list)
+
+selected_orgs = st.sidebar.multiselect("Select Organization(s)", st.session_state.orgs)
+
+selected_places = st.sidebar.multiselect("Select Place(s)", st.session_state.places)
+selected_homelands = st.sidebar.multiselect("Select Homeland(s)", st.session_state.homelands)
+selected_provinces = st.sidebar.multiselect("Select Province(s)", st.session_state.provinces)
+selected_hrvs = st.sidebar.multiselect("Select HRV(s)", st.session_state.hrvs)
 
 dates_checkbox = st.sidebar.checkbox("Use Dates?")
 cols = st.sidebar.columns(2)
 hits_container = st.container()
 dataframe_expander = st.expander("Open to Examine the Data")
-
+metadata_expander = st.expander("Open to Examine Connected Data")
 if dates_checkbox:
     start_date = cols[0].date_input("Start Date", datetime.date(1980, 7, 6),
                                             min_value=datetime.date(1950, 7, 6),
@@ -118,66 +91,95 @@ if dates_checkbox:
 else:
     start_date = 0
     end_date = 0
-res = df
+
+def get_output_data(df):
+    columns = [["org", "orgs"], ["place", "places"], ["homeland", "homelands"], ["province", "provinces"], ["hrv", "hrvs"]]
+    final_dict = {}
+    for column, ss_key in columns:
+        temp_df = df[df[column].notna()]
+        hits = temp_df[column].tolist()
+        uniques = []
+        for hit in hits:
+            for item in hit:
+                if item not in uniques and item != None:
+                    uniques.append(item)
+        uniques.sort()
+        final_dict[column] = uniques
+    formatted_text = ""
+    for item in final_dict:
+        formatted_text = formatted_text+f"**{item.upper()}**(S): {', '.join(final_dict[item])}<br><br>"
+    return formatted_text
+
+def filter_df(df, column, selected_list):
+    final = []
+    for idx, row in df.iterrows():
+        # st.write(type(row[column]))
+        if str(type(row[column])) == "<class 'numpy.ndarray'>":
+            if any(item in selected_list for item in row[column]):
+                final.append(row)
+    res = pd.DataFrame(final)
+    return res
+
+res = full_data
 if selected_orgs:
-    res = df.loc[df.org.isin(selected_orgs)]
+    res = filter_df(full_data, "org", selected_orgs)
+    # st.write(res)
 if selected_places:
-    res = res.loc[res.place.isin(selected_places)]
+    res = filter_df(res, "place", selected_places)
+    # res = res.loc[res.place.isin(selected_places)]
 if selected_homelands:
-    res = res.loc[res.homeland.isin(selected_homelands)]
+    res = filter_df(res, "homeland", selected_homelands)
+    # res = res.loc[res.homeland.isin(selected_homelands)]
 if selected_provinces:
-    res = res.loc[res.province.isin(selected_provinces)]
+    res = filter_df(res, "province", selected_provinces)
+    # res = res.loc[res.province.isin(selected_provinces)]
 if selected_hrvs:
-    res = res.loc[res.hrv.isin(selected_hrvs)]
+    res = filter_df(res, "hrv", selected_hrvs)
+    # res = res.loc[res.hrv.isin(selected_hrvs)]
 
-hit_ids = res.object_id.tolist()
-# st.write(hit_ids)
-# st.write(dates_only[0])
-if dates_checkbox:
-    data = []
-    for item in dates_only:
-        if "date_time" in item and item["id"] in hit_ids:
-            if item["date_time"] > start_date and item["date_time"] < end_date:
-                data.append(copy.copy(item))
-    for item in data:
-        item.pop("date_time")
+# metadata_connections = get_output_data(res)
+# metadata_expander.markdown(metadata_connections, unsafe_allow_html=True)
+if len(res) > 0:
+    hit_ids = res.object_id.tolist()
+    if dates_checkbox:
+        full_data = full_data[full_data['date_time'].notna()]
+        data = full_data.loc[dates_only["object_id"].isin(hit_ids)]
+        data = data.loc[data["date_time"] > start_date and data["date_time"] < end_date]
+    else:
+        data = full_data.loc[full_data["object_id"].isin(hit_ids)]
+    hits_container.write(f"Total Hits: {len(data)}")
+    cols2 = st.columns(2)
+    if len(data) > 0:
+        final_data = json.loads(data[["place", "coordinates"]].to_json())
+        final_df = pd.DataFrame(final_data)
+        # final_df['frequency'] = final_df['place'].map(final_df['place'].value_counts())
+        # final_df["radius"] = final_df["frequency"].apply(lambda quantity: quantity*10)
+
+        layer = pdk.Layer(
+            "ScatterplotLayer",
+            final_df,
+            pickable=True,
+            opacity=0.8,
+            stroked=True,
+            filled=False,
+            radius_scale=6,
+            radius_min_pixels=1,
+            radius_max_pixels=1000,
+            line_width_min_pixels=5,
+            get_position="coordinates",
+            # get_radius="radius",
+            get_fill_color=[255, 140, 0],
+            get_line_color=[255, 140, 0],
+        )
+
+        # Set the viewport location
+        view_state = pdk.ViewState(longitude=25.975520, latitude=-30.50329, zoom=5, bearing=0, pitch=0, height=1000)
+
+        # Render
+        r = pdk.Deck(layers=[layer], initial_view_state=view_state, tooltip={"text": "{place} ({frequency})"})
+        # r.to_html("scatterplot_layer.html")
+        st.pydeck_chart(r)
+        # display_res = res["Last", "First", "Description"]
+        dataframe_expander.dataframe(res)
 else:
-    data = [copy.copy(item) for item in full_data if item["id"] in hit_ids]
-    for item in data:
-        if "date_time" in item:
-            item.pop("date_time")
-    # st.write(data[0])
-hits_container.write(f"Total Hits: {len(data)}")
-cols2 = st.columns(2)
-if len(data) > 0:
-    final_df = pd.DataFrame(data)
-    final_df = final_df[["place", "coordinates"]]
-    final_df['frequency'] = final_df['place'].map(final_df['place'].value_counts())
-    final_df["radius"] = final_df["frequency"].apply(lambda quantity: quantity*10)
-
-    layer = pdk.Layer(
-        "ScatterplotLayer",
-        final_df,
-        pickable=True,
-        opacity=0.8,
-        stroked=True,
-        filled=False,
-        radius_scale=6,
-        radius_min_pixels=1,
-        radius_max_pixels=1000,
-        line_width_min_pixels=5,
-        get_position="coordinates",
-        get_radius="radius",
-        get_fill_color=[255, 140, 0],
-        get_line_color=[255, 140, 0],
-    )
-
-    # Set the viewport location
-    view_state = pdk.ViewState(longitude=25.975520, latitude=-30.50329, zoom=5, bearing=0, pitch=0, height=1000)
-
-    # Render
-    r = pdk.Deck(layers=[layer], initial_view_state=view_state, tooltip={"text": "{place} ({frequency})"})
-    # r.to_html("scatterplot_layer.html")
-    st.pydeck_chart(r)
-    # display_res = res["Last", "First", "Description"]
-    dataframe_expander.dataframe(res)
+    st.markdown("**There are no matches**")
